2981da24a64e54891d14599bec5f4a9e5da281d7
rayhan18 <rabu4960@gmail.com>
2020-12-23 12:54:54 +0300

    Navbar and Footer

---
+++ my-respos/src/App.js
@@ -0,0 +1,16 @@
+import React,{Fragment}from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from "./Component/Home";
+import Footer from "./Component/Footer";
+
+function App() {
+  return (
+    <Fragment>
+          <Home/>
+          <Footer/>
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ my-respos/src/Component/Home.js
@@ -0,0 +1,32 @@
+import React, {Component,Fragment} from 'react';
+import {Container,NavDropdown,Navbar,Nav} from 'react-bootstrap';
+
+class Home extends Component {
+    render() {
+        return (
+            <Fragment>
+                {/*<Container fluid={true}>*/}
+                    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark fixed-top" >
+                        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                        <Navbar.Collapse id="responsive-navbar-nav">
+                            <Nav className="mr-auto">
+                                <Nav.Link href="#features">Features</Nav.Link>
+                                <Nav.Link href="#pricing">Pricing</Nav.Link>
+                               
+                            </Nav>
+                            <Nav>
+                                <Nav.Link href="#deets">More deets</Nav.Link>
+                                <Nav.Link eventKey={2} href="#memes">
+                                    Dank memes
+                                </Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Navbar>
+                {/*</Container>*/}
+            </Fragment>
+        );
+    }
+}
+
+export default Home;
